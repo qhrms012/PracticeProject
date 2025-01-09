52a883d090a8c717a4653bae2dd9e576acab5ef1
boguen <qhrms012@naver.com>
2025-01-09 19:28:36 +0900

    [fix] 수정

--- Assets/01.Scripts/Action/ActionExample.cs
+++ Assets/01.Scripts/Action/ActionExample.cs
@@ -3,14 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActionExample
+public class ActionExample : MonoBehaviour
 {
     public void Start()
     {
-        Action sayHello = () => Console.WriteLine("Hello, Action!");
+        Action sayHello = () => Debug.Log("Hello, Action!");
         sayHello(); // "Hello, Action!" 출력
 
-        Action<int> printNumber = (number) => Console.WriteLine($"Number: {number}");
+        Action<int> printNumber = (number) => Debug.Log($"Number: {number}");
         printNumber(5); // "Number: 5" 출력
+
     }
 }
