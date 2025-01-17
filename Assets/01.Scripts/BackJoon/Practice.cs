dcbb062b3e7d5c0c4634d0818d028a6b7fd16331
boguen <qhrms012@naver.com>
2025-01-17 20:53:19 +0900

    [add] 백준 연습

--- Assets/01.Scripts/BackJoon/Practice.cs
+++ Assets/01.Scripts/BackJoon/Practice.cs
@@ -60,4 +60,98 @@ public class Practice : MonoBehaviour
         Console.WriteLine(((A % C) * (B % C)) % C);
     }
 
+    public void Practice2588()
+    {
+        string a = Console.ReadLine();
+        string b = Console.ReadLine();
+
+        Console.WriteLine(int.Parse(a) * (int)Char.GetNumericValue(b[2]));
+        Console.WriteLine(int.Parse(a) * (int)Char.GetNumericValue(b[1]));
+        Console.WriteLine(int.Parse(a) * (int)Char.GetNumericValue(b[0]));
+        Console.WriteLine(int.Parse(a) * int.Parse(b));
+    }
+
+    public void Practice11382()
+    {
+        string[] s = Console.ReadLine().Split();
+
+        double A = double.Parse(s[0]);
+        double B = double.Parse(s[1]);
+        double C = double.Parse(s[2]);
+        Console.WriteLine(A + B + C);
+    }
+
+    public void Practice10171()
+    {
+        Console.WriteLine("\\    /\\");
+        Console.WriteLine(" )  ( ')");
+        Console.WriteLine("(  /  )");
+        Console.WriteLine(" \\(__)|");
+    }
+    public void Practice10172() 
+    {
+        Console.WriteLine("|\\_/|");
+        Console.WriteLine("|q p|   /}");
+        Console.WriteLine("( 0 )\"\"\"\\");
+        Console.WriteLine("|\"^\"`    |");
+        Console.WriteLine("||_/=\\\\__|");
+    }
+
+    public void Practice1330()
+    {
+        string[] s = Console.ReadLine().Split();
+
+        int A = int.Parse(s[0]);
+        int B = int.Parse(s[1]);
+
+        if (A > B)
+        {
+            Console.WriteLine(">");
+        }
+        else if (A < B)
+        {
+            Console.WriteLine("<");
+        }
+        else
+        {
+            Console.WriteLine("==");
+        }
+    }
+
+    public void Practice9498()
+    {
+        string s = Console.ReadLine();
+
+        int A = int.Parse(s);
+
+        string grade = A switch
+        {
+            >= 90 => "A",
+            >= 80 => "B",
+            >= 70 => "C",
+            >= 60 => "D",
+            _ => "F"
+        };
+
+        Console.WriteLine(grade);
+    }
+
+    public void Pracktice2753()
+    {
+        //string s = Console.ReadLine();
+
+        //int A = int.Parse(s);
+
+        //if (A % 4 == 0 && A % 100 > 0 || A % 400 == 0)
+        //{
+        //    Console.WriteLine("1");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("0");
+        //}
+
+        int A = int.Parse(Console.ReadLine());
+        Console.WriteLine((A % 4 == 0 && A % 100 != 0 || A % 400 == 0) ? "1" : "0");
+    }
 }
