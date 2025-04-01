2c45c9ffa0ec35f20455f60983e62d7fed50866f
boguen <qhrms012@naver.com>
2025-04-01 19:58:56 +0900

    [add] 인벤토리 dropitem 메서드 만들기

--- Assets/01.Scripts/Inventory/Inventory.cs
+++ Assets/01.Scripts/Inventory/Inventory.cs
@@ -21,6 +21,21 @@ public class Inventory : MonoBehaviour
             Debug.Log(item.itemName + " 제거됨!");
         }
     }
+    public void DropItem(ItemData item, Vector3 dropPosition)
+    {
+        if (items.Contains(item))
+        {
+            items.Remove(item);
+            Debug.Log(item.itemName + " 버림!");
+
+            // 아이템 오브젝트 생성
+            GameObject droppedItem = Instantiate(Resources.Load<GameObject>("DroppedItemPrefab"), dropPosition, Quaternion.identity);
+            droppedItem.GetComponent<ItemPickup>().itemData = item;
+
+            // UI 업데이트
+            FindObjectOfType<InventoryUI>().UpdateInventoryUI();
+        }
+    }
 
     public void ShowInventory()
     {
