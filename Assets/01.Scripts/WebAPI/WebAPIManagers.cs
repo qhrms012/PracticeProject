2c56c45abcd75344f910c22a0063f935623452bc
boguen <qhrms012@naver.com>
2025-04-02 19:13:49 +0900

    [add] unity webapi 서버에 data 보내보기

--- Assets/01.Scripts/WebAPI/WebAPIManagers.cs
+++ Assets/01.Scripts/WebAPI/WebAPIManagers.cs
@@ -1,18 +1,38 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using System.Text;
 
 public class WebAPIManagers : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private string apiUrl = "https://jsonplaceholder.typicode.com/posts"; // 예제 API
+
     void Start()
     {
-        
+        StartCoroutine(SendDataToAPI());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SendDataToAPI()
     {
-        
+        string jsonData = "{\"title\":\"foo\",\"body\":\"bar\",\"userId\":1}";
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonData);
+
+        using (UnityWebRequest request = new UnityWebRequest(apiUrl, "POST"))
+        {
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log("서버 응답: " + request.downloadHandler.text);
+            }
+            else
+            {
+                Debug.LogError("에러 발생: " + request.error);
+            }
+        }
     }
 }
