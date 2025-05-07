ff96998cdb5732dfdf09338731823bb9f726130f
boguen <qhrms012@naver.com>
2025-05-07 20:50:36 +0900

    [add] 문제 2444 풀기

--- Assets/01.Scripts/BackJoon/Practice.cs
+++ Assets/01.Scripts/BackJoon/Practice.cs
@@ -894,6 +894,22 @@ public class Practice : MonoBehaviour
         Console.WriteLine("      |");
     }
 
+    public void Practice2444()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(new string(' ', n - i));
+            Console.WriteLine(new string('*', 2 * i - 1));
+        }
+        for (int i = n - 1; i >= 1; i--)
+        {
+            Console.Write(new string(' ', n - i));
+            Console.WriteLine(new string('*', 2 * i - 1));
+        }
+    }
+
 
 }
 
