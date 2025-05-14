3841f4afe27f2373b388bff7922b9937d767baf1
boguen <qhrms012@naver.com>
2025-05-14 21:37:36 +0900

    [add] 2754 학점계산 문제 풀기

--- Assets/01.Scripts/BackJoon/Practice.cs
+++ Assets/01.Scripts/BackJoon/Practice.cs
@@ -1230,5 +1230,31 @@ public class Practice : MonoBehaviour
 
         Console.WriteLine(output);
     }
+    public void Practice2754()
+    {
+        var input = Console.ReadLine();
+        Dictionary<string, double> average = new Dictionary<string, double>()
+            {
+                {"A+",4.3},
+                {"A0",4.0},
+                {"A-",3.7},
+                {"B+",3.3},
+                {"B0",3.0},
+                {"B-",2.7},
+                {"C+",2.3},
+                {"C0",2.0},
+                {"C-",1.7},
+                {"D+",1.3},
+                {"D0",1.0},
+                {"D-",0.7},
+                {"F", 0.0}
+
+            };
+        if (average.ContainsKey(input))
+        {
+            double output = average[input];
+            Console.WriteLine($"{output:F1}");
+        }
+    }
 }
 
