4479dd7f6f23458b4c52268830679d013a9823f9
boguen <qhrms012@naver.com>
2025-05-28 21:03:58 +0900

    [add] 2775부녀회장이 될테야 문제 풀기
    2차원 배열로 접근해서 풀기

--- Assets/01.Scripts/BackJoon/Practice.cs
+++ Assets/01.Scripts/BackJoon/Practice.cs
@@ -1699,5 +1699,37 @@ public class Practice : MonoBehaviour
         }
     }
 
+    public void Practice2775()
+    {
+        int T = int.Parse(Console.ReadLine());
+
+        for (int t = 0; t < T; t++)
+        {
+            int k = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] apt = new int[k + 1, n + 1];
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                apt[0, i] = i;
+            }
+
+
+            for (int i = 1; i <= k; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    apt[i, j] = apt[i, j - 1] + apt[i - 1, j];
+                }
+            }
+
+            Console.WriteLine(apt[k, n]);
+
+
+        }
+    }
+
 }
 
