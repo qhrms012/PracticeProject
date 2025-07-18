c5a701aaf0cee6e116aa264fd937311e35dbc8aa
boguen <qhrms012@naver.com>
2025-07-18 20:57:32 +0900

    [add] 백준 1929 소수구하기 문제 풀기
    2부터 N까지 모든 수를 소수 후보로 둔다.
    
    2부터 시작해서, 그 수의 배수들을 전부 지운다.
    
    지워지지 않은 수는 소수다.
    
    이때 검사할 수는 i * i <= N까지만 확인하면 된다.

--- Assets/01.Scripts/BackJoon/Practice.cs
+++ Assets/01.Scripts/BackJoon/Practice.cs
@@ -2593,6 +2593,42 @@ public class Practice : MonoBehaviour
 
         Console.WriteLine((int)Math.Round(average, MidpointRounding.AwayFromZero));
     }
+
+    public void Practice1929()
+    {
+        string[] input = Console.ReadLine().Split();
+        int m = int.Parse(input[0]);
+        int n = int.Parse(input[1]);
+
+        bool[] isPrime = new bool[n + 1];
+
+        // 일단 전부 소수라고 가정
+        for (int i = 2; i <= n; i++)
+        {
+            isPrime[i] = true;
+        }
+
+        // 에라토스테네스의 체 실행
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (isPrime[i])
+            {
+                for (int j = i * i; j <= n; j += i)
+                {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        // m 이상 n 이하인 소수 출력
+        for (int i = m; i <= n; i++)
+        {
+            if (isPrime[i])
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
 }
     
 
