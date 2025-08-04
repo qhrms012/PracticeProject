084ae8d0b8de20be2f888b2d20afc1db27a5f23e
boguen <qhrms012@naver.com>
2025-08-04 20:45:26 +0900

    [add] 백준 1463 1로 만들기 풀기
    입력값이 주어지면 1이 될 때까지 연산을 수행한다.
    
    각 숫자 i에 대해 최소 연산 횟수 dp[i]를 저장한다.
    
    dp[i]를 구하려면, i-1, i/2, i/3(나누어 떨어질 경우) 중 최소값에 1을 더하면 된다.

--- Assets/01.Scripts/BackJoon/Practice.cs
+++ Assets/01.Scripts/BackJoon/Practice.cs
@@ -3009,6 +3009,27 @@ public class Practice : MonoBehaviour
             Console.WriteLine($"{dp[n, 0]} {dp[n, 1]}");
         }
     }
+
+    public void Practice1463()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] dp = new int[n + 1];
+
+        dp[1] = 0;
+
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] + 1;
+
+            if (i % 2 == 0)
+                dp[i] = Math.Min(dp[i], dp[i / 2] + 1);
+
+            if (i % 3 == 0)
+                dp[i] = Math.Min(dp[i], dp[i / 3] + 1);
+        }
+
+        Console.WriteLine(dp[n]);
+    }
 }
     
 
