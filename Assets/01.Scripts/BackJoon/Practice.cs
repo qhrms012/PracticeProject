3b87cdd847e6759c9ce97e4d8571996493c1559a
boguen <qhrms012@naver.com>
2025-08-22 22:53:28 +0900

    [add] 백준11726 문제 풀기

--- Assets/01.Scripts/BackJoon/Practice.cs
+++ Assets/01.Scripts/BackJoon/Practice.cs
@@ -3258,6 +3258,26 @@ public class Practice : MonoBehaviour
         Console.Write(sb);
     }
 
+    public void Practice11726()
+    {
+        int n = int.Parse(Console.ReadLine()!);
+        const int MOD = 10007;
+
+        if (n == 1) { Console.WriteLine(1); return; }
+        if (n == 2) { Console.WriteLine(2); return; }
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++)
+        {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+
+        Console.WriteLine(dp[n]);
+    }
+
  }
 
 
