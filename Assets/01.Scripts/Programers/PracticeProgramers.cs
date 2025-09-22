0dafcd59fc7d2cd0341c800a6bbc4e14b0f81522
boguen <qhrms012@naver.com>
2025-09-22 19:09:18 +0900

    [add] 12910 프로그래머스 나누어 떨어지는 숫자 배열 문제 풀기

--- Assets/01.Scripts/Programers/PracticeProgramers.cs
+++ Assets/01.Scripts/Programers/PracticeProgramers.cs
@@ -331,4 +331,23 @@ public class PracticeProgramers : MonoBehaviour
 
         //return answer;
     }
+
+    public void Practice12910(int[] arr, int divisor)
+    {
+        List<int> list = new List<int>();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % divisor == 0)
+                list.Add(arr[i]);
+        }
+
+        if (list.Count == 0)
+            list.Add(-1);
+        else
+            list.Sort();
+
+        //return list.ToArray();
+    }
+
 }
