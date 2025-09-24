b735d0eddd4ad048d8c602f5246a3e527d9101ea
boguen <qhrms012@naver.com>
2025-09-24 17:20:12 +0900

    [add] 12943 프로그래머스 문제 콜라츠 추측 풀기

--- Assets/01.Scripts/Programers/PracticeProgramers.cs
+++ Assets/01.Scripts/Programers/PracticeProgramers.cs
@@ -367,4 +367,23 @@ public class PracticeProgramers : MonoBehaviour
 
        // return answer;
     }
+
+    public void Practice12943(int num)
+    {
+        int answer = 0;
+        long a = num;
+
+        while (a > 1 && answer < 500)
+        {
+            if (a % 2 == 0)
+                a = a / 2;
+            else
+                a = a * 3 + 1;
+            answer++;
+        }
+        if (answer >= 500)
+            answer = -1;
+
+       // return answer;
+    }
 }
