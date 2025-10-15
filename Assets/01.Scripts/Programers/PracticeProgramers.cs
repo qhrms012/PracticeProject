09dcc709f653ad5d04241288b26c2d3f50344b9a
boguen <qhrms012@naver.com>
2025-10-15 20:24:00 +0900

    [add] 프로그래머스 행렬의 덧셈 문제 풀기

--- Assets/01.Scripts/Programers/PracticeProgramers.cs
+++ Assets/01.Scripts/Programers/PracticeProgramers.cs
@@ -512,4 +512,22 @@ public class PracticeProgramers : MonoBehaviour
 
         //return answer;
     }
+
+    public void Practice12950(int[,] arr1, int[,] arr2)
+    {
+        int row = arr1.GetLength(0);
+        int col = arr1.GetLength(1);
+
+        int[,] answer = new int[row, col];
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                answer[i, j] = arr1[i, j] + arr2[i, j];
+            }
+        }
+
+       // return answer;
+    }
 }
