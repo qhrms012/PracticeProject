870d4984150e2a9d34f33594abe63e16398ea548
boguen <qhrms012@naver.com>
2025-10-23 20:15:20 +0900

    [add] 프로그래머스 삼총사 문제 풀기

--- Assets/01.Scripts/Programers/PracticeProgramers.cs
+++ Assets/01.Scripts/Programers/PracticeProgramers.cs
@@ -626,4 +626,25 @@ public class PracticeProgramers : MonoBehaviour
         //return answer;
     }
 
+    public void Practice131705(int[] number)
+    {
+        int answer = 0;
+
+        for (int i = 0; i < number.Length - 2; i++)
+        {
+            for (int j = i + 1; j < number.Length - 1; j++)
+            {
+                for (int k = j + 1; k < number.Length; k++)
+                {
+                    if (number[i] + number[j] + number[k] == 0)
+                    {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        //return answer;
+    }
+
 }
