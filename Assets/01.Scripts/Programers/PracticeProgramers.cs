e30950938b6071ec6ce7e3714cdaf5d17ff8cce7
boguen <qhrms012@naver.com>
2025-10-30 19:36:58 +0900

    [add] 프로그래머스 가장 가까운 같은 글자 문제 풀기

--- Assets/01.Scripts/Programers/PracticeProgramers.cs
+++ Assets/01.Scripts/Programers/PracticeProgramers.cs
@@ -696,4 +696,28 @@ public class PracticeProgramers : MonoBehaviour
 
        // return maxWidth * maxHeight;
     }
+
+    public void Practice142086(string s)
+    {
+        int[] answer = new int[s.Length];
+        Dictionary<char, int> lastIndex = new Dictionary<char, int>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            char c = s[i];
+
+            if (lastIndex.ContainsKey(c))
+            {
+                answer[i] = i - lastIndex[c];
+                lastIndex[c] = i;
+            }
+            else
+            {
+                answer[i] = -1;
+                lastIndex[c] = i;
+            }
+        }
+
+        //return answer;
+    }
 }
