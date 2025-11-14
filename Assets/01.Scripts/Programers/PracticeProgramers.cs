4829b115ddb30d1f7aa9dba9bf07c77aa670c4dc
boguen <qhrms012@naver.com>
2025-11-14 21:03:33 +0900

    [add] 프로그래머스 각도기 문제 풀기

--- Assets/01.Scripts/Programers/PracticeProgramers.cs
+++ Assets/01.Scripts/Programers/PracticeProgramers.cs
@@ -886,4 +886,16 @@ public class PracticeProgramers : MonoBehaviour
 
         //return answer;
     }
+
+    public void Practice120829(int angle)
+    {
+        int answer = 0;
+
+        if (angle < 90) answer = 1;
+        else if (angle == 90) answer = 2;
+        else if (angle < 180) answer = 3;
+        else if (angle == 180) answer = 4;
+
+        //return answer;
+    }
 }
