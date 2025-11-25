b5a62d5f9b6c5c48527222c36cb88b5851cdf501
boguen <qhrms012@naver.com>
2025-11-25 19:06:08 +0900

    [add] 프로그래머스 배열자르기 문제 풀기

--- Assets/01.Scripts/Programers/PracticeProgramers.cs
+++ Assets/01.Scripts/Programers/PracticeProgramers.cs
@@ -997,4 +997,13 @@ public class PracticeProgramers : MonoBehaviour
 
         return sum > max ? 1 : 2;
     }
+
+    public int[] Practice120833(int[] numbers, int num1, int num2)
+    {
+        int length = num2 - num1 + 1;
+        int[] answer = new int[length];
+        Array.Copy(numbers, num1, answer, 0, length);
+
+        return answer;
+    }
 }
