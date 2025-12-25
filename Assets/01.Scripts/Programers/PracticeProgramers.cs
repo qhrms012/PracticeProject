cd56bcb4f726972e824f2f6e10a33ecd3414d544
boguen <qhrms012@naver.com>
2025-12-25 20:02:03 +0900

    [add] 프로그래머스 옷가게 할인받기 문제 풀기

--- Assets/01.Scripts/Programers/PracticeProgramers.cs
+++ Assets/01.Scripts/Programers/PracticeProgramers.cs
@@ -1282,4 +1282,22 @@ public class PracticeProgramers : MonoBehaviour
                 answer.Add(i);
         return answer.ToArray();
     }
+
+    public int Practice120818(int price)
+    {
+        int answer = price;
+        if (price >= 500000)
+        {
+            answer = (int)(price * 0.8);
+        }
+        else if (price >= 300000)
+        {
+            answer = (int)(price * 0.9);
+        }
+        else if (price >= 100000)
+        {
+            answer = (int)(price * 0.95);
+        }
+        return answer;
+    }
 }
