69c7564d109bae349a7e1e760dd07796d43c7a61
boguen <qhrms012@naver.com>
2026-01-20 22:20:00 +0900

    [add] 프로그래머스 배열 회전시키기 문제 풀기

--- Assets/01.Scripts/Programers/PracticeProgramers.cs
+++ Assets/01.Scripts/Programers/PracticeProgramers.cs
@@ -1510,4 +1510,29 @@ public class PracticeProgramers : MonoBehaviour
 
         return answer;
     }
+
+    public int[] Practice120844(int[] numbers, string direction)
+    {
+        int len = numbers.Length;
+        int[] answer = new int[len];
+
+        if (direction == "right")
+        {
+            answer[0] = numbers[len - 1];
+            for (int i = 0; i < len - 1; i++)
+            {
+                answer[i + 1] = numbers[i];
+            }
+        }
+        else
+        {
+            answer[len - 1] = numbers[0];
+            for (int i = 1; i < len; i++)
+            {
+                answer[i - 1] = numbers[i];
+            }
+        }
+
+        return answer;
+    }
 }
